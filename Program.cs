cbac3d82f6a66b127e6516b87ffe2266e1db1f92
huseyin.gokdemir <huseyin.gokdemir@agarthateknoloji.com.tr>
2022-11-17 22:56:44 +0300

    Coalescing Operator

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = null;
+        
+        name = name ?? "Varsayılan Ad";
+
+        Console.WriteLine(name);
+    }
+}
